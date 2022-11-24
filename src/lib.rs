6819ec3e09acecf575fade45881b646104f55aa2
kuqmua <serezhashmakov@outlook.com>
2022-11-24 20:31:01 +0300

    restucture clippy warnings

--- src/lib.rs
+++ src/lib.rs
@@ -31,36 +31,12 @@ pub fn derive_enum_extension(input: proc_macro::TokenStream) -> proc_macro::Toke
     let name = &ast.ident;
     let gen = quote::quote! {
         impl #name {
-            #[deny(
-                clippy::indexing_slicing,
-                clippy::unwrap_used,
-                clippy::integer_arithmetic,
-                clippy::float_arithmetic
-            )]
             pub fn get_length() -> usize {
                 #len
             }
-            #[deny(
-                clippy::indexing_slicing,
-                clippy::unwrap_used,
-                clippy::integer_arithmetic,
-                clippy::float_arithmetic
-            )]
             pub fn into_array() -> [#name; #len] {
                 [ #(#name::#variants),* ]
             }
-            #[deny(
-                clippy::indexing_slicing,
-                clippy::unwrap_used,
-                clippy::integer_arithmetic,
-                clippy::float_arithmetic
-            )]
-            #[deny(
-                clippy::indexing_slicing,
-                clippy::unwrap_used,
-                clippy::integer_arithmetic,
-                clippy::float_arithmetic
-            )]
             pub fn into_vec() -> Vec<Self> {
                 use strum::IntoEnumIterator;
                 let mut self_vec = Vec::with_capacity(Self::get_length());
@@ -69,12 +45,6 @@ pub fn derive_enum_extension(input: proc_macro::TokenStream) -> proc_macro::Toke
                 }
                 self_vec
             }
-            #[deny(
-                clippy::indexing_slicing,
-                clippy::unwrap_used,
-                clippy::integer_arithmetic,
-                clippy::float_arithmetic
-            )]
             pub fn into_string_name_and_variant_hashmap() -> std::collections::HashMap<String, Self> {
                 use strum::IntoEnumIterator;
                 let mut variants_hashmap: std::collections::HashMap<String, Self> =
@@ -84,12 +54,6 @@ pub fn derive_enum_extension(input: proc_macro::TokenStream) -> proc_macro::Toke
                 }
                 variants_hashmap
             }
-            #[deny(
-                clippy::indexing_slicing,
-                clippy::unwrap_used,
-                clippy::integer_arithmetic,
-                clippy::float_arithmetic
-            )]
             pub fn into_string_name_and_variant_tuple_vec() -> Vec<(String, Self)> {
                 use strum::IntoEnumIterator;
                 let mut variants_vec = Vec::with_capacity(Self::get_length());
@@ -98,29 +62,11 @@ pub fn derive_enum_extension(input: proc_macro::TokenStream) -> proc_macro::Toke
                 }
                 variants_vec
             }
-            #[deny(
-                clippy::indexing_slicing,
-                clippy::unwrap_used,
-                clippy::integer_arithmetic,
-                clippy::float_arithmetic
-            )]
-            #[deny(
-                clippy::indexing_slicing,
-                clippy::unwrap_used,
-                clippy::integer_arithmetic,
-                clippy::float_arithmetic
-            )]
             pub fn to_upper_snake_case(&self) -> String {
                 use convert_case::Casing;
                 use convert_case;
                 format!("{:?}", self).to_case(convert_case::Case::Snake).to_uppercase()
             }
-            #[deny(
-                clippy::indexing_slicing,
-                clippy::unwrap_used,
-                clippy::integer_arithmetic,
-                clippy::float_arithmetic
-            )]
             pub fn to_lower_snake_case(&self) -> String {
                 use convert_case::Casing;
                 use convert_case;
