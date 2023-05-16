86f4470384a29df93d21a570afa7e0f54ec13531
kuqmua <serezhashmakov@outlook.com>
2023-05-17 00:26:49 +0300

    panic_location()

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@
 /// convert_case = "0.6.0"
 #[proc_macro_derive(EnumExtension)]
 pub fn enum_extension(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    proc_macro_helpers::panic_location::panic_location("EnumExtension");
+    proc_macro_helpers::panic_location::panic_location();
     //it only supported for enums without values
     let ast: syn::DeriveInput = syn::parse(input).expect("EnumExtension syn::parse(input) failed");
     //todo to implement into_array() and into_vec - must implement Default for all inner variant types
