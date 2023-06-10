0061ad7193d456265b8c883c86334999d051f710
kuqmua <serezhashmakov@outlook.com>
2023-06-10 22:25:33 +0300

    strum version = "=0.24.1"

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@
 #![allow(clippy::too_many_arguments)]
 
 ///you need to install this crates to use this macro(check on work was only with this versions)
-/// strum = "0.24.1"
+/// strum = {version = "=0.24.1"}
 /// strum_macros = "0.24.3"
 /// convert_case = "0.6.0"
 #[proc_macro_derive(EnumExtension)]
