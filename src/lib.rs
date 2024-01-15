7ccc7a1f09f4afa430687cdbf546c6ec565ebd7e
kuqmua <serezhashmakov@outlook.com>
2024-01-15 22:30:34 +0300

    next steps

--- src/lib.rs
+++ src/lib.rs
@@ -82,12 +82,12 @@ pub fn enum_extension(input: proc_macro::TokenStream) -> proc_macro::TokenStream
                 variants_vec
             }
             //todo - it can be done in compile time
-            pub fn to_upper_snake_case(&self) -> String {
-                convert_case::Casing::to_case(&format!("{:?}", self),convert_case::Case::Snake).to_uppercase()
+            pub fn to_upper_camel_case(&self) -> String {
+                convert_case::Casing::to_case(&format!("{:?}", self),convert_case::Case::UpperCamel)
             }
             //todo - it can be done in compile time
-            pub fn to_lower_snake_case(&self) -> String {
-                convert_case::Casing::to_case(&format!("{:?}", self),convert_case::Case::Snake).to_lowercase()
+            pub fn to_snake_case(&self) -> String {
+                convert_case::Casing::to_case(&format!("{:?}", self),convert_case::Case::Snake)
             }
         }
     };
